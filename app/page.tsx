e3b11e3f0d38d8010acdb804f9adea77eb915cd8
LukwagoJoel <99560679+lukwagojoel@users.noreply.github.com>
2026-09-03 17:41:04 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 
 import { ScrollExperience } from "@/components/kprstyle/Effects/scrollExp";
-import {  MaintenanceScreen } from "@/components/kprstyle/Site";
 import React from "react";
 
 export default function Home() {
   return (
-    // <main className="min-h-screen bg-black text-white selection:bg-fuchsia-400 selection:text-black">
-    //   <ScrollExperience />
-    // </main>
-    <MaintenanceScreen/>
+    <main className="min-h-screen bg-black text-white selection:bg-fuchsia-400 selection:text-black">
+      <ScrollExperience />
+    </main>
   );
 }
